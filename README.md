92ad4e8ef5ceb787958acc1b2c59c8f5275da371
robsonsilv4 <robsonsilv410@gmail.com>
2020-03-03 19:21:41 -0300

    init: projeto inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Quero Workar Mobile
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quero Workar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
