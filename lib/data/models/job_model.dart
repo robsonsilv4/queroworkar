e6ea0da7982eb2e5fb80c1afd99f86399d348754
robsonsilv4 <robsonsilv410@gmail.com>
2025-09-24 17:13:07 -0300

    feat: improve job model with equatable

--- lib/data/models/job_model.dart
+++ lib/data/models/job_model.dart
@@ -1,7 +1,10 @@
-class Job {
-  Job({
+import 'package:equatable/equatable.dart';
+
+class Job extends Equatable {
+  const Job({
     required this.image,
     required this.title,
+    required this.description,
     required this.date,
     required this.url,
   });
@@ -9,19 +12,33 @@ class Job {
   factory Job.fromJson(Map<String, dynamic> json) => Job(
         image: json['image'] as String,
         title: json['title'] as String,
+        description: json['description'] as String,
         date: json['date'] as String,
         url: json['url'] as String,
       );
 
-  String image;
-  String title;
-  String date;
-  String url;
+  static const empty = Job(
+    image: '',
+    title: '',
+    description: '',
+    date: '',
+    url: '',
+  );
+
+  final String image;
+  final String title;
+  final String description;
+  final String date;
+  final String url;
 
   Map<String, dynamic> toJson() => {
         'image': image,
         'title': title,
+        'description': description,
         'date': date,
         'url': url,
       };
+
+  @override
+  List<Object?> get props => [image, title, description, date, url];
 }
