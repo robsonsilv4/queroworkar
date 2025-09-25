b54786a55a28915f24ea1abc0bdcc093ba3527a2
robsonsilv4 <robsonsilv410@gmail.com>
2025-09-25 17:32:42 -0300

    feat: add back dio caching support

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:get_it/get_it.dart';
 import 'package:quero_workar/blocs/jobs/jobs.dart';
 import 'package:quero_workar/data/repositories/repositories.dart';
@@ -6,19 +8,24 @@ final GetIt sl = GetIt.instance;
 
 void setupLocator() {
   sl
+    ..registerLazySingleton<Dio>(
+      () {
+        final cacheOptions = CacheOptions(
+          store: MemCacheStore(),
+          hitCacheOnErrorCodes: [500],
+          hitCacheOnNetworkFailure: true,
+          maxStale: const Duration(days: 2),
+        );
+        return Dio()
+          ..interceptors.add(DioCacheInterceptor(options: cacheOptions));
+      },
+    )
     ..registerLazySingleton<JobRepository>(
       () {
         return JobInMemoryRepository();
         // Uncomment below to use HTML repository
-        // final dio = Dio();
-        // dio.interceptors.add(
-        //   DioCacheManager(
-        //     CacheConfig(baseUrl: Api.baseUrl),
-        //   ).interceptor as Interceptor,
-        // );
-
         // return JobHtmlRepository(
-        //   client: dio,
+        //   client: sl.get<Dio>(),
         // );
       },
     )
